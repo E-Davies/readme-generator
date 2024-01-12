4f811097754a06bd4717ae3d06323a3f7f459aae
e-davies <emmi.lou002@gmail.com>
2024-01-12 19:56:49 +0000

    added questions and created structure of generateMarkdown

--- index.js
+++ index.js
@@ -1,20 +1,73 @@
 const fs = require("fs");
-const path = require('path');
+//const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What does the user need to know about how to install this?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using this repo?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What license does your project have?',
+        choices: ['MIT', 'Apache', 'BSD', 'GPL'],
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to this project?',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'What command should be used to run tests?',
+      },
+      {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub username',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address',
+      },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(fileName, answers) {
+    //generateMarkdown()
+
+    fs.writeFile('README.md', answers, (err) => 
+    err ? console.error(err) : console.log('Success!'))
 }
 
 // function to initialize program
 function init() {
-
+    inquirer
+        .prompt(questions)
+        .then((answers) =>{ 
+            console.log(answers)
+        });
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,30 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `# ${answers.title}
+  
+# Description
+${answers.description} 
 
+## Table of Contents 
+ 
+
+## Installation 
+${answers.installation} 
+
+## Usage 
+${answers.usage} 
+
+## License 
+${answers.license} 
+
+## Contributing 
+${answers.contributing} 
+
+## Tests 
+${answers.tests} 
+
+## Questions
+If you have any questions regarding this repo, open an issue or contact me directly at ${answers.email}. You can find more of my work at [${answers.githubUsername}](https://github.com/${answers.githubUsername}/).
 `;
 }
 
